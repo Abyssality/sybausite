8efb77cd0e1b74129d6cf6456ca000ec3ad26362
Abyssality <abyssalityxd@gmail.com>
2025-05-18 22:23:58 +0300

    Update opengraph-image.tsx

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,6 +1,6 @@
-import { ImageResponse } from "next/og"
+import { ImageResponse } from "next/og";
 
-export const runtime = "edge"
+export const runtime = "edge";
 
 export async function GET() {
   return new ImageResponse(
@@ -94,6 +94,6 @@ export async function GET() {
     {
       width: 1200,
       height: 630,
-    },
-  )
+    }
+  );
 }
